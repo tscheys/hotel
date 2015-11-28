7880f3c25dfa9241152f495afc281dd47b28e642
tscheys <tim.scheys@gmail.com>
2015-11-27 23:02:28 -0800

    add script tag for factory constants

--- client/controllers/makepriceoffer/MakepriceofferCtrl.js
+++ client/controllers/makepriceoffer/MakepriceofferCtrl.js
@@ -4,43 +4,13 @@
   angular.module('hotelApp')
   .controller('MakepriceofferCtrl', MakepriceofferCtrl);
 
-  MakepriceofferCtrl.$inject = ['pdfFactory'];
+  MakepriceofferCtrl.$inject = ['pdfFactory', 'constantFactory'];
 
-  function MakepriceofferCtrl(pdfFactory) {
+  function MakepriceofferCtrl(pdfFactory, constantFactory) {
     var makepriceoffer = this;
     
     makepriceoffer.createOffer = function () {
-      var data = {
-        naam: makepriceoffer.naam,
-        nights: makepriceoffer.nachten,
-        babybed: makepriceoffer.babybed,
-        persons: [{
-          title: 'Volwassenen in tweepersoonskamer',
-          number: makepriceoffer.volwkoppel,
-          price: 44
-         },
-         {
-          title: 'Volwassenen in familiekamer',
-          number: makepriceoffer.volwfam,
-          price: 41
-         },
-         {
-          title: 'Kinderen jonger dan 12 jaar',
-          number: makepriceoffer.ki12,
-          price: 28
-         },
-         {
-          title: 'Kinderen jonger dan 6 jaar',
-          number: makepriceoffer.ki6,
-          price: 25
-         },
-         {
-          title: 'Babies',
-          number: makepriceoffer.babies,
-          price: 12
-         }
-        ]};
-      pdfFactory.priceofferPdf(data);
+      pdfFactory.priceofferPdf(constantFactory.priceoffer(makepriceoffer));
     }
   }
 })();
