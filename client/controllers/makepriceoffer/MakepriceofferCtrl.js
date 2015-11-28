08381e9fd2ba1d17bd48ee6893411c8c1631377c
tscheys <tim.scheys@gmail.com>
2015-11-27 19:52:59 -0800

    make priceoffer object complete

--- client/controllers/makepriceoffer/MakepriceofferCtrl.js
+++ client/controllers/makepriceoffer/MakepriceofferCtrl.js
@@ -13,6 +13,7 @@
       var data = {
         naam: makepriceoffer.naam,
         nights: makepriceoffer.nachten,
+        babybed: makepriceoffer.babybed,
         persons: [{
           title: 'Volwassenen in tweepersoonskamer',
           number: makepriceoffer.volwkoppel,
@@ -22,6 +23,21 @@
           title: 'Volwassenen in familiekamer',
           number: makepriceoffer.volwfam,
           price: 41
+         },
+         {
+          title: 'Kinderen jonger dan 12 jaar',
+          number: makepriceoffer.ki12,
+          price: 28
+         },
+         {
+          title: 'Kinderen jonger dan 6 jaar',
+          number: makepriceoffer.ki6,
+          price: 25
+         },
+         {
+          title: 'Babies',
+          number: makepriceoffer.babies,
+          price: 12
          }
         ]};
       pdfFactory.priceofferPdf(data);
