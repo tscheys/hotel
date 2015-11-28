a84b21be9e104a45515343338145c411d7bc7e29
tscheys <tim.scheys@gmail.com>
2015-11-27 19:21:44 -0800

    implement basic price offer controller

---
+++ client/controllers/priceoffers/PriceoffersCtrl.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular.module('hotelApp')
+  .controller('PriceoffersCtrl', PriceoffersCtrl);
+
+  function PriceoffersCtrl() {
+    var self = this;
+
+    
+  }
+
+})();
