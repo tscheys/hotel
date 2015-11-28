9c500f530a382eb499f2ea71f305dba5de8bafcd
tscheys <tim.scheys@gmail.com>
2015-11-27 19:22:25 -0800

    add basic pdf layout to price offer

--- client/factories/pdfFactory.js
+++ client/factories/pdfFactory.js
@@ -12,9 +12,58 @@
     return services;
 
     function priceofferPdf (inquiryData) {
-      var docDefinition = { content: inquiryData.naam };
-      pdfMake.createPdf(docDefinition).open();
+      var price = calculatePrice(inquiryData);
+      pdfMake.createPdf(docDefinition(inquiryData, price)).open();
     }
   }
 
 })();
+
+function calculatePrice (data) {
+  var total = 0;
+  var summary = [];
+  data.persons.forEach(function(clientType) {
+    summary.push(clientType.title + ': ' + clientType.number + ' x ' + clientType.price + ' = ' + clientType.price * clientType.number);
+    total += clientType.price * clientType.number;
+  });
+  summary.push('Totaal per nacht: ' + total);
+  summary.push('Aantal nachten: ' + data.nights);
+  summary.push('Totaal verblijf: ' + total * data.nights);
+  return summary;
+}
+
+function docDefinition (data, price) {
+  return {
+    content: [
+          { text: 'Prijsofferte', style: 'header' },
+          'Beste ,', 'Onderaan dit document vindt u de totaalprijs voor uw verblijf.',
+          { text: 'Overzicht', style: 'subheader' },
+          price
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10]
+      },
+      subheader: {
+        fontSize: 16,
+        bold: true,
+        margin: [0, 10, 0, 5]
+      },
+      tableExample: {
+        margin: [0, 5, 0, 15]
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 13,
+        color: 'black'
+      }
+    },
+    defaultStyle: {
+      // alignment: 'justify'
+    }
+  }
+};
+
+
